d4915dd612c2a67631326b0bee549a53ca44c7d6
RyanPSN <ryangregoire286@gmail.com>
2024-10-01 19:54:11 -0400

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        RichTextBox1 = New RichTextBox()
+        RichTextBox2 = New RichTextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(100, 359)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(156, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox1.Location = New Point(39, 125)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(278, 37)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox2.Location = New Point(39, 206)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(278, 37)
+        RichTextBox2.TabIndex = 2
+        RichTextBox2.Text = ""
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(11, 437)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Label1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(363, 466)
+        Controls.Add(Label1)
+        Controls.Add(RichTextBox2)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load()
+
+    End Sub
+
+
+
+    Function Add(ByVal a, ByVal b)
+        Return a + b
+
+    End Function
+
+    Function Subtract(ByVal a, ByVal b)
+        Return a - b
+
+    End Function
+
+
+    Function Num()
+
+        Dim M As System.Random = New Random()
+
+        Return M.Next(0, 10)
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim UserName As String = RichTextBox1.Text
+        Dim Password As String = RichTextBox2.Text
+
+        Dim Num1 = Num()
+        Dim Num2 = Num()
+
+        If (UserName.Equals("Admin") And Password.Equals("Admin")) Then
+            Label1.Text = "Signed In as: " + UserName
+            Form2.Show()
+
+        End If
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox2.TextChanged
+
+    End Sub
+End Class
